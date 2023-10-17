b316b5044976eaffe3d25e50c0865f79efd7aca1
Latifus <saitbaevlatif@gmail.com>
2023-10-17 11:09:51 +0200

    first commit

---
+++ src/main/java/com/example/platform_mvp/PlatformMvpApplication.java
@@ -0,0 +1,13 @@
+package com.example.platform_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformMvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformMvpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/platform_mvp/PlatformMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.platform_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformMvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
